2a2e5c40e32e18516a1204586916dddc214310dd
newdongyuwei <newdongyuwei@localhost>
2011-08-04 17:41:09 +0800

    eval code;auto hide notification

--- try-ruby-on-websocket.rb
+++ try-ruby-on-websocket.rb
@@ -1,7 +1,7 @@
 #! /usr/bin/ruby
 #author newdongyuwei@gmail.com
 
-%w(rubygems sinatra sinatra/base eventmachine  em-websocket stringio fakefs/safe ).each{|lib|require lib}
+%w(rubygems sinatra sinatra/base eventmachine  em-websocket stringio ).each{|lib|require lib}
 
 class WebServer < Sinatra::Base
     set  :run, true
@@ -24,60 +24,64 @@ class WebServer < Sinatra::Base
 		<br>
 		<textarea id="log" style="width:500px;height:200px"></textarea>
 		<script>
-                function Notifier(){}
-
-                Notifier.prototype.RequestPermission = function(cb){
-                    window.webkitNotifications.requestPermission(function(){
-                        if (cb) {
-                            cb(window.webkitNotifications.checkPermission() == 0);
-                        }
-                    });
-                }
-
-                Notifier.prototype.Notify = function(icon, title, body){
-                    if (window.webkitNotifications.checkPermission() === 0) {
-                        var popup = window.webkitNotifications.createNotification(icon, title, body);
-                        popup.show();
-                        return true;
-                    }
-                    return false;
-                }
-
-                var notifier = new Notifier();
-                document.getElementById("request-permission").onclick = function(){
-                    notifier.RequestPermission();
-                };
-                
-				if (window.WebSocket) {
-				    var ws = new WebSocket("ws://host:7777".replace("host",window.location.hostname));
-				    
-				    ws.onopen = function(){
-				        ws.send("puts \"web socket demo\" ");
-						setInterval(function(){
-    							if(ws.readyState === 1  && ws.bufferedAmount === 0){
-      								ws.send("KeepAlive");
-							}
-  						}, 20000);
-				    };
-				    
-				    ws.onmessage = function(evt){
-				        var data = evt.data;
-				        console.log(data);
-                        notifier.Notify("http://www.google.com.hk/intl/zh-CN/images/logo_cn.png", "WebSocket message:", data)
-		    			var log = document.getElementById("log");
-			    		log.value = data;
-				    };
-				    
-				    ws.onclose = function(){
-				        console.log("socket closed");
-				    };
-					
-				    document.getElementById("send").onclick = function(){
-				        ws.send(document.getElementById("content").value.split("\n").join(";"));
-				    };
-				}else {
-				    alert("You browser does not support  web sockets,try google chrome");
-				};
+			    function Notifier(){}
+			
+			    Notifier.prototype.RequestPermission = function(cb){
+			        window.webkitNotifications.requestPermission(function(){
+			            if (cb) {
+			                cb(window.webkitNotifications.checkPermission() == 0);
+			            }
+			        });
+			    }
+			
+			    Notifier.prototype.Notify = function(icon, title, body){
+			        if (window.webkitNotifications.checkPermission() === 0) {
+			            var popup = window.webkitNotifications.createNotification(icon, title, body);
+			            popup.show();
+			setTimeout(function(){
+				popup.cancel && popup.cancel();
+			}, 5000);//默认5s自动隐藏
+			
+			        return true;
+			    }
+			    return false;
+			}
+			
+			var notifier = new Notifier();
+			document.getElementById("request-permission").onclick = function(){
+			    notifier.RequestPermission();
+			};
+			
+			if (window.WebSocket) {
+			    var ws = new WebSocket("ws://host:7777".replace("host",window.location.hostname));
+			
+			ws.onopen = function(){
+			    ws.send("puts \"web socket demo\" ");
+				setInterval(function(){
+						if(ws.readyState === 1  && ws.bufferedAmount === 0){
+							ws.send("KeepAlive");
+					}
+				}, 20000);
+			};
+			
+			ws.onmessage = function(evt){
+			    var data = evt.data;
+			    console.log(data);
+			    notifier.Notify("http://www.google.com.hk/intl/zh-CN/images/logo_cn.png", "WebSocket message:", data)
+				var log = document.getElementById("log");
+				log.value = data;
+			};
+			
+			ws.onclose = function(){
+			    console.log("socket closed");
+			};
+			
+			document.getElementById("send").onclick = function(){
+			    ws.send(document.getElementById("content").value.split("\n").join(";"));
+			    };
+			}else {
+			    alert("You browser does not support  web sockets,try google chrome");
+			};
 		</script>
         '
 	end
@@ -87,35 +91,30 @@ EM.kqueue = true if EM.kqueue?
 EM.epoll = true if EM.epoll?
 
 EventMachine.run {
-    WebServer.run!
-    EventMachine::WebSocket.start(:host => "0.0.0.0",:port => 7777) do |ws|
-        ws.onopen {
-          puts "WebSocket connection open"
-        }
-        ws.onmessage do |msg|
-            puts "WebSocket Recieved message: #{msg}"
-	    FakeFS.activate!
-            begin
-		if msg != "KeepAlive"
-			Thread.start{
-				EventMachine.system('ruby','-e',"$SAFE = 3 ; #{msg}") do |out,status|
-                    			puts status.exitstatus
-                   	 		if out.strip != "" 
-						ws.send(out)
+	WebServer.run!
+	EventMachine::WebSocket.start(:host => "0.0.0.0",:port => 7777) do |ws|
+		ws.onopen {
+			puts "WebSocket connection open"
+		}
+		ws.onmessage do |msg|
+			if msg != "KeepAlive"
+				puts "WebSocket Recieved message: #{msg}" 
+				Thread.start{
+					$SAFE = 3 
+					begin
+						$stdout = StringIO.new
+						eval(msg)
+						ws.send( $stdout.string)
+					rescue
+						ws.send($!.to_s) 
+					ensure
+						 $stdout = STDOUT 
 					end
-                		end
-			}
+					#ws.send(eval("$SAFE = 3 ; begin $stdout = StringIO.new; #{msg}; $stdout.string; rescue ws.send($!.to_s) ;ensure $stdout = STDOUT; end"))
+				}
+			end
 		end
-	    rescue SecurityError
-		ws.send("You aren't allowed to run that command!")
-	    rescue Error => e
-                puts 'error: #{e}'
-                ws.send($!.to_s)
-	    ensure
-		FakeFS.deactivate!
-            end
-          
-        end
-        ws.onclose { puts "WebSocket Connection closed" }
-    end
+		ws.onclose { puts "WebSocket Connection closed" }
+	end
 }
+
